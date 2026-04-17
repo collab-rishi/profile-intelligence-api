85a73b79f351be9fcd46ca516093618c65a9aced
collab-rishi <convey.rishi@gmail.com>
2026-04-18 02:21:41 +0530

    fix: add binaryTargets to prisma schema for linux compatibility

--- src/index.ts
+++ src/index.ts
@@ -36,8 +36,8 @@ app.use((req, res, next) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(Number(PORT), "0.0.0.0", () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 export default app;
